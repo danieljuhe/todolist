0df865b3626e8174e960431f88ca8e2779d39052
dani <danieljuhe@gmail.com>
2022-11-13 23:23:06 +0100

    deleting todos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,34 +1,40 @@
 import React, { useState } from "react";
-import '../../styles/index.css'
+import '../../styles/index.css';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [input, setInput]=useState();
-	const [save, setSave]=useState();
+	const [inputControler, setInputControler]=useState();
+	const [todos, setTodos]=useState([]);
 
 	const inputvalue = (event) => {
-		setInput(event.target.value)
+		setInputControler(event.target.value)
 	}
 
 	const keypress = (event) => {
 		if (event.key === 'Enter'){
-		setSave(input)
-		document.getElementById('myInput').value = ''
+			setTodos(prev => [...prev,inputControler])
+			setInputControler("")
 		}
 	} 
-	
+
+	const erase = (event) => {
+		setTodos([])
+	}
 	
 	return (
 		<div className="main">
 			<div className="toodo">
 				<div className="title"><p className="ptitle">toodolist</p></div>
 				<div className="imputdiv">
-					<input type="text" id="myInput" className="input" onChange={inputvalue} onKeyDown={keypress}></input>
-					<div className="first"><h5 id="temporalvalue">{input}</h5></div>
-					<div className="second"><h1>{save}</h1></div>
+					<input type="text" className="input" value={inputControler} onChange={inputvalue} onKeyDown={keypress}></input>
+					<div className="second">{todos.map((todo,index)=>{
+						return (
+							<div className="divh4"><h4 key={index}>{todo}<button className="button" onClick={erase}>X</button></h4></div>
+						)
+					})}</div>
 				</div>
 			</div>
 		</div>
