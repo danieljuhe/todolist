fa95319152853420214f56c2102c79c4ea5bfe2a
dani <danieljuhe@gmail.com>
2022-11-15 22:13:43 +0100

    add mouseover mouseout

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,6 +8,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [inputControler, setInputControler]=useState();
 	const [todos, setTodos]=useState([]);
+	const [button, setButton]=useState("");
 
 	const inputvalue = (event) => {
 		setInputControler(event.target.value)
@@ -25,6 +26,14 @@ const Home = () => {
 		setTodos(filteredTodos);
 	}
 
+	const buttonblock = (event) => {
+		setButton("appear")
+	}
+
+	const buttonhidden = () => {
+		setButton("")
+	}
+
 
 	return (
 		<div className="main">
@@ -34,9 +43,9 @@ const Home = () => {
 					<input type="text" className="input" value={inputControler} onChange={inputvalue} onKeyDown={keypress}></input>
 					<div className="second">{todos.map((todo,index)=>{
 							return (
-								<div key={index} className="divh4">
+								<div key={index} className="divh4" onMouseOver={buttonblock} onMouseOut={buttonhidden}>
 									<h4>{todo}</h4>
-									<button className="button" id={todo} onClick={erase}> X </button>
+									<button className="button" key={index} id={todo} onClick={erase}  style={{display: (button=="appear") ? 'block':'none',}}> X </button>
 								</div>
 							)
 						})}
